accc16bb42976e96dbf3826e495b2c911a998cab
root <vega@cryptofarm.wiki>
2020-01-14 12:42:58 +0000

    Rails 5.1

--- db/migrate/201209260800_add_sla_to_issue_categories.rb
+++ db/migrate/201209260800_add_sla_to_issue_categories.rb
@@ -1,4 +1,4 @@
-class AddSlaToIssueCategories < ActiveRecord::Migration
+class AddSlaToIssueCategories < ActiveRecord::Migration[4.2]
   def change
     add_column :issue_categories, :sla_default, :integer, :default => 0
     add_column :issue_categories, :sla_vip,     :integer, :default => 0
